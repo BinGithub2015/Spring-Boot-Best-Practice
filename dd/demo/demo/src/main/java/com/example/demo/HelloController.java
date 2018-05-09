96a90192bab17b3234d05cb9e92dba5762f7f05f
bin <178933579@qq.com>
2018-05-10 00:13:12 +0800

    test

--- dd/demo/demo/src/main/java/com/example/demo/HelloController.java
+++ dd/demo/demo/src/main/java/com/example/demo/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +11,14 @@ public class HelloController {
     @RequestMapping("/hello")
     public String index() {
         return "Hello World";
+    }
+
+    @RequestMapping("/test")
+    public String test(){
+
+        MongoClient mongoClient = new MongoClient("");
+         MongoDatabase database = mongoClient.getDatabase("");
+        return "test";
     }
 
 }
