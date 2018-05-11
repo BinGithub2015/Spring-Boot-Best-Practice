1f62944999020ae433803cb0d5262ad7505d90e3
bin <178933579@qq.com>
2018-05-12 01:11:55 +0800

    最佳实践

---
+++ bestpractice/bestpractice/src/main/java/com/bestpractice/com/bestpractice/BestpracticeApplication.java
@@ -0,0 +1,12 @@
+package com.bestpractice.com.bestpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestpracticeApplication.class, args);
+	}
+}
