82d243335b1ebabff77e7a02ba834d6e7952d6e1
chenjunbin <178933579@qq.com>
2018-05-30 17:51:36 +0800

    redis

--- demo/demo/src/main/java/com/example/demo/aspect/AuthAspect.java
+++ demo/demo/src/main/java/com/example/demo/aspect/AuthAspect.java
@@ -3,17 +3,27 @@ package com.example.demo.aspect;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.annotation.Resource;
 import javax.servlet.http.Cookie;
 
 @Aspect
 @Component
 @Order(2)
 public class AuthAspect {
+
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
+    @Resource(name="stringRedisTemplate")
+    ValueOperations<String,String> valOpsStr;
 
     @Pointcut("execution(public * com.example.demo.controller.*.*.*(..))")
     public void auth() {
@@ -23,6 +33,12 @@ public class AuthAspect {
     public void before(){
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         Cookie[] cookies = attributes.getRequest().getCookies();
+        for (Cookie cookie:cookies) {
+            if(cookie.getName().equals("user_uuid")){
+                String uuid = cookie.getValue();
+                valOpsStr.get("user:");
+            }
+        }
         //todo：权限判断
         System.out.println("权限判断");
 
