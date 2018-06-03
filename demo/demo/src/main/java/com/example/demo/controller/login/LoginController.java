39611c6cdc73251464ae2345246768001f42c9c0
bin <178933579@qq.com>
2018-06-03 17:26:29 +0800

    rest

--- demo/demo/src/main/java/com/example/demo/controller/login/LoginController.java
+++ demo/demo/src/main/java/com/example/demo/controller/login/LoginController.java
@@ -3,6 +3,10 @@ package com.example.demo.controller.login;
 import com.example.demo.controller.base.BaseController;
 import com.example.demo.controller.base.Response;
 import com.example.demo.controller.base.StateCode;
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/login")
 public class LoginController extends BaseController {
 
-    @RequestMapping(value = "/", method = RequestMethod.POST)
-    public Response post() {
-        return response(StateCode.SUCCESS, new Object());
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public Response post(@RequestBody Integer id) {
+        User user = userService.findById(id);
+        if(user == null) return response(StateCode.FAIL, "不存在该用户");
+        else return response(StateCode.SUCCESS, user);
     }
 
 }
