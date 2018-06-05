d6043cab31c592589a6a0c43b425afbbcf51120c
bin <178933579@qq.com>
2018-06-05 23:30:19 +0800

    db

--- demo/demo/src/main/java/com/example/demo/controller/login/LoginController.java
+++ demo/demo/src/main/java/com/example/demo/controller/login/LoginController.java
@@ -6,10 +6,7 @@ import com.example.demo.controller.base.StateCode;
 import com.example.demo.model.User;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/login")
@@ -18,8 +15,8 @@ public class LoginController extends BaseController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value = "", method = RequestMethod.POST)
-    public Response post(@RequestBody Integer id) {
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public Response post(@RequestParam Integer id) {
         User user = userService.findById(id);
         if(user == null) return response(StateCode.FAIL, "不存在该用户");
         else return response(StateCode.SUCCESS, user);
