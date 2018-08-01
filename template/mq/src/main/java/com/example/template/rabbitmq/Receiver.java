719fb289b75414b55abe80c4b6ece09d2d69e2a1
bin <178933579@qq.com>
2018-08-01 23:23:27 +0800

    fix:rabbitmq

--- template/mq/src/main/java/com/example/template/rabbitmq/Receiver.java
+++ template/mq/src/main/java/com/example/template/rabbitmq/Receiver.java
@@ -1,9 +1,14 @@
 package com.example.template.rabbitmq;
 
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Receiver {
 
+    @RabbitListener(queues = "")
+    public void receive(String in) throws InterruptedException {
+        //do some things
+    }
 
 }
--- template/mq/src/main/java/com/example/template/rabbitmq/Sender.java
+++ template/mq/src/main/java/com/example/template/rabbitmq/Sender.java
@@ -2,12 +2,20 @@ package com.example.template.rabbitmq;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class Sender {
 
     @Autowired
     private RabbitTemplate rabbitTemplate;
+    @Value("")
+    String exchange;
+    @Value("")
+    String routingKey;
 
-    public Sender() {
+    public void sender(Object object) {
+        rabbitTemplate.convertAndSend(exchange,routingKey,object);
     }
 }
