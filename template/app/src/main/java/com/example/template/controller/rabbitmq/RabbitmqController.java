a9ff1275b60651c63db6d4234ccb9678e04e0419
bin <178933579@qq.com>
2018-08-02 22:58:06 +0800

    fix:rabbitmq

---
+++ template/app/src/main/java/com/example/template/controller/rabbitmq/RabbitmqController.java
@@ -0,0 +1,24 @@
+package com.example.template.controller.rabbitmq;
+
+import com.example.template.common.model.response.Response;
+import com.example.template.controller.AbstractController;
+import com.example.template.rabbitmq.Sender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/rabbitmq")
+public class RabbitmqController extends AbstractController {
+
+    @Autowired
+    Sender sender;
+
+    @RequestMapping(value = "/sender/{msg}",method = RequestMethod.GET)
+    public String sender(String msg){
+        sender.sender(msg);
+        return "ok";
+    }
+
+}
