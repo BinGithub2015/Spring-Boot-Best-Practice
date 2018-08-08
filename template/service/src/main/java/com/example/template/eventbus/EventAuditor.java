e79e4f12e0d1d2799760fbd27a63ef360a0c427b
bin <178933579@qq.com>
2018-08-08 22:41:09 +0800

    feat:guava

---
+++ template/service/src/main/java/com/example/template/eventbus/EventAuditor.java
@@ -0,0 +1,33 @@
+package com.example.template.eventbus;
+
+import com.example.template.common.utilities.EventBusUtilties;
+import com.google.common.eventbus.AllowConcurrentEvents;
+import com.google.common.eventbus.DeadEvent;
+import com.google.common.eventbus.Subscribe;
+import org.jboss.logging.Logger;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class EventAuditor {
+
+    private Logger logger = Logger.getLogger(EventAuditor.class);
+
+    @PostConstruct
+    private void init(){
+        EventBusUtilties.register(this);
+    }
+
+    @Subscribe
+    @AllowConcurrentEvents
+    private void auditString(String msg){
+        logger.info(msg);
+    }
+
+    @Subscribe
+    public void handleUnsubscribedEvent(DeadEvent deadEvent){
+        logger.warn("No subscribers for "+deadEvent.getEvent());
+    }
+
+}
