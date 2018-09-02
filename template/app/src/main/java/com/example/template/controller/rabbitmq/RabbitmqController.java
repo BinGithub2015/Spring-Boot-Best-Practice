496c691d7c71a26ac767dd03c8d4da159cc08a4d
bin <178933579@qq.com>
2018-09-02 23:56:12 +0800

    fix:actuator

--- template/app/src/main/java/com/example/template/controller/rabbitmq/RabbitmqController.java
+++ template/app/src/main/java/com/example/template/controller/rabbitmq/RabbitmqController.java
@@ -3,8 +3,6 @@ package com.example.template.controller.rabbitmq;
 import com.example.template.common.constant.ResponseCode;
 import com.example.template.common.model.response.Response;
 import com.example.template.controller.AbstractController;
-import com.example.template.rabbitmq.Sender;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,12 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/rabbitmq")
 public class RabbitmqController extends AbstractController {
 
-    @Autowired
-    private Sender sender;
+//    @Autowired
+//    private Sender sender;
 
     @RequestMapping(value = "/sender/{msg}",method = RequestMethod.GET)
     public Response sender(@PathVariable String msg){
-        sender.sender(msg);
+        //sender.sender(msg);
         return response(ResponseCode.SUCCESS,null);
     }
 
